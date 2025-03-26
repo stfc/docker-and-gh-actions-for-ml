d0fd904c7f861e8f80d66f50636b56bc200bcc8a
Drew Silcock <drew.silcock@stfc.ac.uk>
2025-03-26 17:28:59 +0000

    Update create SP script and add example azure.env file.

--- create-sp.sh
+++ create-sp.sh
@@ -8,6 +8,7 @@
 # - tenant_id
 # - subscription_id
 # - resource_group
+# See `azure.env.example` for details.
 source ./azure.env
 
 # TODO: Restrict scopes to only `/providers/Microsoft.Web/sites` and `/providers/Microsoft.Web/serverfarms`.
@@ -16,4 +17,17 @@ scopes="/subscriptions/$subscription_id/resourceGroups/$resource_group"
 sp_name="supercharge-workshop"
 
 set -x
-az ad sp create-for-rbac --role="$role" --scopes="$scopes" --name="$sp_name"
+sp_creds="$(az ad sp create-for-rbac --role="$role" --scopes="$scopes" --name="$sp_name")"
+
+app_id="$(echo $sp_creds | jq -r '.appId')"
+password="$(echo $sp_creds | jq -r '.password')"
+
+env_vars='
+tenant_id="'$tenant_id'"
+subscription_id="'$subscription_id'"
+sp_username="'$app_id'"
+sp_password="'$password'"
+resource_group="'$resource_group'"
+'
+
+echo $env_vars
